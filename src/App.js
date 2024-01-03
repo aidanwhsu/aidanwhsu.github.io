c4f741077df1529a260b65637e66f241139986d0
aidanwhsu <133727326+aidanwhsu@users.noreply.github.com>
2024-01-03 16:21:21 -0500

    adding more react stuff

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Content from "./components/content.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
